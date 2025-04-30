1bc7fddace402e3e03f092d12c47eb0292a80e2a
abdulhafeez <105729069+Abdl-Hafeez@users.noreply.github.com>
2025-05-01 01:38:02 +0300

    ad id to tasks

--- src/task.js
+++ src/task.js
@@ -1,9 +1,12 @@
 export class Task {
+    static taskId = 1;
     constructor(title, description, dueDate, priority, status) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
         this.status = status;
+        this.id = `${this.title}-${Task.taskId++}`;
     }
 }
+
